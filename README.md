b3bd5ea7c40e726504db78dc1c36ddb0c4f388ca
eduardo machado <eduardoshy1@gmail.com>
2023-01-13 09:30:18 -0300

    config readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # configuration-terminal
-Repositório criado para facilitar a configuração do terminal no linux.  
+
+Repositório criado para facilitar a configuração do terminal no linux.
+
+# Importante
+
+Apesar do script efetuar o download de tudo que é necessário, para ativar é necessário que você manualmente adicione algumas coisas ao seu arquivo .zshrc que será criado após a instalação do ohmyzsh.
+
+# Fast-syntax-highlight
+
+Após o download do plugin fast-syntax-highlight, é necessário adicionar a seguinte linha dentro do seu arquivo .zshrc:
+`source ~/path/to/fsh/F-Sy-H.plugin.zsh`
+
+# Zsh-autosuggestions
+
+Após o download do plugin Zsh-autosuggestions, é necessário adicionar a seguinte linha dentro do seu arquivo .zshrc:
+
+```plugins=(
+    # other plugins...
+    zsh-autosuggestions
+)
+```
+
+# Zsh-completions
+
+Após o download do plugin Zsh-completions, é necessário adicionar a seguinte linha dentro do seu arquivo .zshrc, logo em baixo da linha com source `"$ZSH/oh-my-zsh.sh"`:
+
+```
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+```
+
+# powerlevel10k
+
+Após o download do tema powerlevel10k, é necessário adicionar a seguinte linha dentro do seu arquivo .zshrc:
+`ZSH_THEME="powerlevel10k/powerlevel10k`
---
+++ configuration_terminal.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#author: Eduardo Machado 
+
+echo "Bem vindo a configuração"
+sleep 4s
+clear
+
+echo "Instalando zsh..."
+echo "--------------------------"
+sudo apt install zsh
+echo "Download finalizado"
+sleep 4s
+clear
+
+echo "Instalando o curl..."
+echo "-------------------------"
+sudo apt install curl
+echo "Download finalizado"
+sleep 4s
+clear
+
+
+echo "instalando o ohmyzsh..."
+echo "--------------------------------"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Download finalizado"
+sleep 4s 
+clear 
+
+
+echo "Instalando o plugin fast-syntax-highlight..."
+echo "--------------------------------"
+git clone https://github.com/z-shell/F-Sy-H ~/path/to/fsh
+echo "Download finalizado"
+#Adicione este comando ao seu .zshrc: source ~/path/to/fsh/F-Sy-H.plugin.zsh
+sleep 4s
+clear
+
+echo "Instalando o plugin zsh-autosuggestions..."
+echo "--------------------------------"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Download finalizado"
+#Adicione o zsh-autosuggestions na sua lista de plugins no arquivo .zshrc
+sleep 4s
+clear 
+
+echo "Instalando o plugin zsh-completions.."
+echo "--------------------------------"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+echo "Download finalizado"
+#Adicione o fpath em seu arquivo .zshrc, logo após a linha de: source "$ZSH/oh-my-zsh.sh"
+sleep 4s
+clear 
+
+echo "Instalando o powerlevel10k"
+echo "--------------------------------"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#Defina sua variável ZSH_THEME como recebendo o valor de powerlevel10k/powerlevel10k no seu arquivo .zshrc
+echo "Download finalizado"
+sleep 4s
+clear 
+
+
+echo "Espero que esse script tenha sido útil. Até a próxima!!!"
+
+
+
+
+ 
